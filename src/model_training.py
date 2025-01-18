3a0dcbcd77b745e601a61e21b259c0718d52df33
Patryk Kłosiński <klosu10@gmail.com>
2025-01-18 18:41:25 +0100

    .

--- src/model_training.py
+++ src/model_training.py
@@ -7,7 +7,7 @@ from autogluon.tabular import TabularPredictor
 
 # Funkcja do ładowania i przetwarzania danych
 def load_data(file_path: str) -> pd.DataFrame:
-    from data_preprocessing import load_and_flatten_json, one_hot_encode_list_columns, extract_unique_values
+    from data_preprocessing import load_and_flatten_json, one_hot_encode_list_columns
 
     # Wczytanie i przetworzenie danych
     df = load_and_flatten_json(file_path)
@@ -26,10 +26,8 @@ def load_data(file_path: str) -> pd.DataFrame:
                        'supported_languages', 'full_audio_languages']
     df.drop(columns=columns_to_drop, axis=1, inplace=True)
 
-    # Zamiana zakresu "estimated_owners" na średnią wartość
-    df['estimated_owners'] = df['estimated_owners'].str.split(' - ').apply(
-        lambda x: (int(x[0].replace(',', '')) + int(x[1].replace(',', ''))) // 2 if isinstance(x, list) else 0
-    )
+    # Zamiana zakresów "estimated_owners" na klasy
+    df = transform_estimated_owners(df)
 
     # Wykonanie one-hot encoding dla kolumn listowych
     list_columns = ['categories', 'genres']
@@ -37,16 +35,31 @@ def load_data(file_path: str) -> pd.DataFrame:
 
     return df
 
+# Funkcja do przekształcenia kolumny estimated_owners w klasy
+def transform_estimated_owners(df):
+    # Definicja przedziałów i etykiet klas
+    bins = [0, 20000, 50000, 100000, 200000, 500000, 1000000, 2000000, 5000000, 10000000, 20000000, 50000000, 100000000, 200000000]
+    labels = [
+        "0-20k", "20k-50k", "50k-100k", "100k-200k", "200k-500k",
+        "500k-1M", "1M-2M", "2M-5M", "5M-10M", "10M-20M",
+        "20M-50M", "50M-100M", "100M-200M"
+    ]
+
+    # Rozbijanie zakresów i zamiana na wartości średnie
+    df['estimated_owners'] = df['estimated_owners'].str.split(' - ').apply(
+        lambda x: (int(x[0].replace(',', '')) + int(x[1].replace(',', ''))) // 2 if isinstance(x, list) else 0
+    )
+    # Przypisywanie do klas
+    df['estimated_owners'] = pd.cut(df['estimated_owners'], bins=bins, labels=labels, include_lowest=True)
+    return df
 
 # Ścieżka do pliku JSON
-file_path = os.path.join(os.getcwd(), 'data/games.json')
+file_path = os.path.join(os.getcwd(), '../data/games.json')
 
 # Wczytanie danych
 print("Wczytywanie danych...")
 df = load_data(file_path)
 print("Dane wczytane i przetworzone.")
-
-print(df.head())
 
 # Przygotowanie cech (X) i etykiety (y)
 X = df.drop('estimated_owners', axis=1)
@@ -58,7 +71,7 @@ train_data, test_data = train_test_split(X, test_size=0.2, random_state=42)
 
 # Trenowanie modelu za pomocą AutoGluon
 print("Rozpoczęcie trenowania modelu za pomocą AutoGluon...")
-predictor = TabularPredictor(label='estimated_owners', eval_metric='r2', problem_type='regression').fit(
+predictor = TabularPredictor(label='estimated_owners', eval_metric='accuracy', problem_type='multiclass').fit(
     train_data=train_data,
     time_limit=700  # Ustaw limit czasu na trenowanie (w sekundach)
 )
