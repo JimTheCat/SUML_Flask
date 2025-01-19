7d8c68813a957afcea700bdb77650221a2d23f84
Patryk Kłosiński <klosu10@gmail.com>
2025-01-19 18:31:39 +0100

    Update configuration

--- flask_app.py
+++ flask_app.py
@@ -100,4 +100,5 @@ def new_game():
                            categories=categories)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8000)
